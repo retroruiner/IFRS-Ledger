ce7d198b342696ff49d59958449100307c499ef1
retroruiner <jostonfer@gmail.com>
2025-08-15 11:56:16 +0600

    added Balance Article and Balance Group

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class BalanceArticle(models.Model):
+    """Статья баланса"""
+
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = "Статья баланса"
+        verbose_name_plural = "Статьи баланса"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class BalanceGroup(models.Model):
+    """Балансовая группа"""
+
+    article = models.ForeignKey(
+        BalanceArticle, on_delete=models.PROTECT, related_name="groups"
+    )
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        unique_together = ("article", "name")
+        verbose_name = "Балансовая группа"
+        verbose_name_plural = "Балансовые группы"
+
+    def __str__(self) -> str:
+        return f"{self.article} / {self.name}"
