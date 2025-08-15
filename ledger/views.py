186dcf363a88f913285abdc0a896cedf5ef1d538
retroruiner <jostonfer@gmail.com>
2025-08-15 12:21:07 +0600

    added account list and transaction list views

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Account, Transaction
+
+
+def accounts_list(request):
+    accounts = Account.objects.select_related("group", "group__article").all()
+    return render(request, "ledger/accounts_list.html", {"accounts": accounts})
+
+
+def transactions_list(request):
+    txs = Transaction.objects.select_related("debit_account", "credit_account").all()
+    return render(request, "ledger/transactions_list.html", {"transactions": txs})
